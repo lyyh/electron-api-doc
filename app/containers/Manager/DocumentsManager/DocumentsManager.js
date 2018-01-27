2ad78bb04715e5bcd86631287e5dff9218aa355e
liuyanhao <374659635@qq.com>
2018-01-27 23:43:03 +0800

    文档管理初步搭建

---
+++ app/containers/Manager/DocumentsManager/DocumentsManager.js
@@ -0,0 +1,16 @@
+/**
+ * @author liuyanhao
+ * @date 2018-01-27
+ * @Description:
+ */
+import React,{Component} from 'react'
+import { Link } from 'react-router-dom';
+import { Menu, Icon, Button,Row, Col } from 'antd';
+class DocManagerContainer extends Component{
+  render(){
+    return(
+      <div>DocManagerContainer</div>
+    )
+  }
+}
+export default DocManagerContainer
