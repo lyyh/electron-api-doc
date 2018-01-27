14bd30a3c040419127e47656bbe5c72d50978bed
liuyanhao <374659635@qq.com>
2018-01-27 13:50:28 +0800

    主线程脚本修改

--- app/main.dev.js
+++ app/main.dev.js
@@ -56,7 +56,7 @@ app.on('window-all-closed', () => {
 
 app.on('ready', async () => {
   if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-    await installExtensions();
+    // await installExtensions();
   }
 
   mainWindow = new BrowserWindow({
