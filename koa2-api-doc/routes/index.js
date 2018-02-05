dc922c8b61edb2cf1efc0e32f981c365edfbb72c
liuyanhao <374659635@qq.com>
2018-02-05 20:41:43 +0800

    user controller router

--- koa2-api-doc/routes/index.js
+++ koa2-api-doc/routes/index.js
@@ -1,11 +1,14 @@
 const router = require('koa-router')()
-
+const UserController = require('../app/controllers/userController')
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
+router.post('/signup',UserController.signup)
+router.post('/signIn',UserController.signIn)
+router.get('/user/key/:id',UserController.getUserByKey)
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
--- koa2-api-doc/routes/users.js
+++ koa2-api-doc/routes/users.js
@@ -6,6 +6,7 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
+
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
