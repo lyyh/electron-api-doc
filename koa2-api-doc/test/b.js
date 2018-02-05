76b874c1ccd43da7930e5655b774a61b4d79ff80
liuyanhao <374659635@qq.com>
2018-02-05 20:40:50 +0800

    test

---
+++ koa2-api-doc/test/b.js
@@ -0,0 +1,17 @@
+/**
+ * @author liuyanhao
+ * @date 2018-02-01
+ * @Description:
+ */
+// export const a = 1
+// export default {
+//   b:1
+// }
+// exports.b = 1
+module.exports = {
+  b: 1,
+  foo: function(){
+    a++
+    console.log(a)
+  }
+}
